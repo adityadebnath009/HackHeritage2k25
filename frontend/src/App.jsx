86dae7f4d1746a0c45cf60504e4ba707304529c3
AVISHIKTA-HAJRA <avishikta9474@gmail.com>
2025-08-13 14:48:49 +0530

    Test commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home.jsx"; // Importing Home component
-// import Chatbot from "./components/Chatbot.jsx";
+import Chatbot from "./components/Chatbot.jsx";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        {/* <Route path="/chatbot" element={<Chatbot />} /> */}
+        <Route path="/chatbot" element={<Chatbot />} />
       </Routes>
     </Router>
   );
